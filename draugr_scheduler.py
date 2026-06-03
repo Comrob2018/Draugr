7a9a827616537a9894e00bdf554081d8917f21a6
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2026-06-03 07:22:15 -0500

    Remove List from import statements

--- draugr_scheduler.py
+++ draugr_scheduler.py
@@ -16,7 +16,7 @@ import subprocess
 import sys
 import time
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, Optional
 
 
 _DEFAULT_SCHEDULER_CONFIG: Dict[str, Any] = {
